c7b71d58620654bce16e92cdea8f5ac5bf93abde
jkrosby51 <56936867+jkrosby51@users.noreply.github.com>
2019-11-08 14:13:25 -0500

    Added descriptions for all functions

--- portableButtonServo.ino
+++ portableButtonServo.ino
@@ -5,19 +5,18 @@
 
 Servo myServo;
 
-int noMove = 0;
-int clockwise = 0;
-int counter = 0;
-int delayVar = 0;
+int noMove = 0; //variable for when the servo is not moving
+int clockwise = 0; //variable for when the servo is moving clockwise
+int counter = 0; //variable for when the servo is moving counter clockwise
+int delayVar = 0; //how long each delay lasts
 int servoPin = 8;
 int buttonPin1 = 4;
 int buttonPin2 = 5;
-int val1 = 0;
-int val2 = 0;
-int servoAngle = 0;
+int val1 = 0; //whether or not button1 is on
+int val2 = 0; //whether or not button2 is on
+int servoAngle = 0; //variable used for servo speed
 
 void setup() {
-
   noMove = 1500;
   clockwise = 1700;
   counter = 1300;
@@ -42,35 +41,39 @@ void loop() {
   Serial.print("         ");
   Serial.print(servoAngle);
   Serial.println(" = servoAngle");
-
+//if only button 1 is on the servo will turn clockwise and show in Serial Monitor
   if (val1 == HIGH && val2 == LOW) {
     //Button 1 on
     servoAngle = clockwise;
     myServo.writeMicroseconds(clockwise);
     delay(delayVar);
   }
+  //if neither buttons pressed do nothing
   else if (val1 == LOW && val2 == LOW) {
     servoAngle = noMove;
     myServo.writeMicroseconds(noMove);
     delay(delayVar);
   }
+  //if both buttons pressed do nothing
   else if (val1 == HIGH && val2 == HIGH) {
     servoAngle = noMove;
     myServo.writeMicroseconds(noMove);
     delay(delayVar);
   }
-
+//if only button 2 is on the servo will turn counter clockwise and show in Serial Monitor
   if (val1 == LOW && val2 == HIGH) {
     //Button 2 on
     servoAngle = counter;
     myServo.writeMicroseconds(counter);
     delay(delayVar);
   }
+  //if neither buttons pressed do nothing
   else if (val1 == LOW && val2 == LOW) {
     servoAngle = noMove;
     myServo.writeMicroseconds(noMove);
     delay(delayVar);
   }
+  //if both buttons pressed do nothing
   else if (val1 == HIGH && val2 == HIGH) {
     servoAngle = noMove;
     myServo.writeMicroseconds(noMove);
@@ -78,6 +81,8 @@ void loop() {
   }
 
 }
+//References
+
 //https://www.arduino.cc/en/tutorial/pushbutton
 //https://create.arduino.cc/projecthub/SBR/working-with-two-leds-and-two-push-buttons-1d4182       wiring
 //https://create.arduino.cc/projecthub/glowascii/button-arduino-basics-5ecffc       more wiring
